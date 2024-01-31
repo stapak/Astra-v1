4ccd8746b2a0fd467f54dad11258ca08ce4bb40f
Sachin Kamat <125467248+stapak@users.noreply.github.com>
2024-01-31 21:05:07 +0530

    added patients to user branch

--- patients.py
+++ patients.py
@@ -11,7 +11,9 @@ class patient:
             session_diagnostic VARCHAR(10000) NOT NULL,
             medications VARCHAR(1000),
             treatment VARCHAR(1000),
-            check_Up DATE
+            check_Up DATE,
+            diagnos_report VARCHAR(1000),
+            diagnos_keypoints VARCHAR(100)
             );
 
          """
@@ -19,12 +21,14 @@ class patient:
 
         
 
-    def session_result(self,patient_name,date,diagnostic,medications,treatment,check_Up):
-        query="""
-        INSERT INTO {patient_name}
-        (session_date,session_diagnostic,medivations,treatment)
+    def session_result(self,**data):
+        result=result
+        query=f"""
+        INSERT INTO {data['name']}
+        (session_date,session_diagnostic,medications,treatment,check_up,diagnos_report,diagnos_keypoints)
         VALUES
-        (table_name,date,diagnostic,medications,treatment,check_up);
+        ({result['date']},{result['diagnostic']},{result['medication']},
+        );
         """
         self.cursor_object.execute(query)
 
@@ -32,7 +36,14 @@ class patient:
         # This function adds patients details to the patient list and creates a personal table to track patients medical history 
         query="""
         INSERT INTO patient_list
+<<<<<<< Updated upstream
         (patient_id,patient_name,phone_no,blood_group,initial_diagnostic,doctor_name)
+=======
+        (patient_id,patient_name,patient_DOB,phone_no,blood_group,initial_diagnostic,doctor_name)
+        VALUES
+        ({patient_info['name']},{patient_info['DOB']},{patient_info['phone no']},{patient_info['blood group']},{patient_info['intial diagnostic']},{patient_info['doctor name']}
+        );
+>>>>>>> Stashed changes
         """
         self.cursor_object.execute(query)
         self._patient_personal(patient_name[name])
@@ -45,3 +56,36 @@ class patient:
         self.cursor_object.execute(query)
         patient_history=self.cursor_object
         return patient_history
+<<<<<<< Updated upstream
+=======
+    
+    def add_diagnos_result(self,**data):
+        query=f"""
+        INSERT INTO patient_list
+        (patient_id,patient_name,patient_DOB,phone_no,blood_group,initial_diagnostic,doctor_name)
+        VALUES
+        ({data['name']},{patient_info['DOB']},{patient_info['phone no']},{patient_info['blood group']},{patient_info['intial diagnostic']},{patient_info['doctor name']}
+        );
+        """
+        pass
+
+
+
+
+
+if __name__=='__main__':
+    Database=mysql.connector.connect(host="localhost",
+                                     user="root",
+                                     passwd="admin",
+                                     auth_plugin='mysql_native_password',
+                                     database="test")
+    cursorobj=Database.cursor()
+    patient=Patient(cursorobj)
+    patient_info={"name":"","DOB":"","phone no":"",'blood_group':"","intial diagnostic":"",
+                  'doctor name':''}
+    for i in patient_info:
+        print(f"Enter {i} of patient")
+        patient_info[i]=input()
+    
+    patient.register(**patient_info)
+>>>>>>> Stashed changes
--- users.py
+++ users.py
@@ -1,2 +1,21 @@
 import mysql.connector
 
+class User:
+    def __init__(self):
+        self.authority={
+            # authority related to adding and removing users
+            "add_doctor":False,
+            "remove_doctor":False,
+
+            "add_receptionist":False,
+            "remove_receptionist":False,
+
+            "add_diagnostic_center":False,
+            "remove_diagnostic_center":False,
+
+            "add_pharmacists":False,
+            "add_pharmacists":False
+
+            # authority to 
+            
+        }
