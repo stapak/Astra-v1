d346b6763eaf8d6c45025bf18cab0e71d5254025
stapak <125467248+stapak@users.noreply.github.com>
2024-05-22 15:39:26 +0530

    Completed licencse activation page, cleared the error of the second page.

--- Software_UI/setup_window.py
+++ Software_UI/setup_window.py
@@ -84,8 +84,10 @@ class Terms_condition_page(Frame):
         #---------------1st part:Company logo and name --------------------------------------------------------
         name_frame=Frame(self,width=550,height=70,bg="#7CB9E8")
         name_frame.place(x=0,y=0)
-        company_name=Label(name_frame,text="Astra Softwares",font=("Arial",30,"bold"),bg="#7CB9E8")
-        company_name.place(x=20,y=10)
+        company_name=Label(name_frame,text="Astra Softwares",font=("Arial",20,"bold"),bg="#7CB9E8")
+        company_name.place(x=20,y=5)
+        page_short_discription=Label(name_frame,text="License Agreement",bg="#7CB9E8",font=(10))
+        page_short_discription.place(x=20,y=40)
         
         image=Image.open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'software_icon.png'))
         resize_image=image.resize((100,60))
@@ -99,16 +101,16 @@ class Terms_condition_page(Frame):
         TC_detailed_frame=Frame(self,width=550,height=290,bg="#DCDCDC")
         TC_detailed_frame.place(y=71,x=0)
         head_label=Label(TC_detailed_frame,text="Terms and Conditon of Use",font=("Arial",10,"bold"),bg="#DCDCDC")
-        head_label.place(y=4,x=10)   
+        head_label.place(y=4,x=25)   
     
-        scroll_box=Frame(TC_detailed_frame,bg="light blue",width=530,height=230,relief="ridge")   
-        scroll_box.place(y=24,x=11) 
+        scroll_box=Frame(TC_detailed_frame,bg="light blue",width=500,height=200,relief="ridge")   
+        scroll_box.place(y=24,x=25) 
 
         scroll_bar_object=Scrollbar2(scroll_box)
-        scroll_bar_object.place(x=513,y=0,height=230)
-            
+        scroll_bar_object.place(x=483,y=0,height=200)
+        
         terms_and_condition_listbox=Listbox(scroll_box,yscrollcommand=scroll_bar_object.set)#main content box
-        terms_and_condition_listbox.place(y=0,x=0,height=230,width=515)
+        terms_and_condition_listbox.place(y=0,x=0,height=200,width=483)
 
         scroll_bar_object.config(command=terms_and_condition_listbox.yview)
 
@@ -118,6 +120,10 @@ class Terms_condition_page(Frame):
 
         
         #----------------------fucntion to change button state-----------------------------------------------
+        
+        quesiton_label=Label(TC_detailed_frame,text="If you agree to the terms and conditon please tick the checkbox",bg="#DCDCDC")
+        quesiton_label.place(y=230,x=25)   
+    
         check_var=IntVar()
         
         
@@ -128,8 +134,8 @@ class Terms_condition_page(Frame):
                 next_button.config(state=tk.DISABLED)
         
         agreeing_checkbutton=Checkbutton2(TC_detailed_frame,text="I agree to the condition",onvalue=1,
-                                          offvalue=0,variable=check_var,command=change_state)
-        agreeing_checkbutton.place(y=260,x=10)
+                                          offvalue=0,variable=check_var,command=change_state,takefocus=1)
+        agreeing_checkbutton.place(y=260,x=25)
 
         #-----------------------3rd part:Button settings-------------------------------------------------------------------
         
@@ -153,37 +159,52 @@ class Software_activation_page(Frame):
         super().__init__(window_object,bg="light grey",width=550,height=400)
         self.pack(fill="x")
 
-        #-----------------------1st part : Entry widget---------------------------------------------------------------
-
-        activation_page_body=Frame(self,bg="#DCDCDC",width=550,height=350)
-        activation_page_body.place(x=0,y=0)
-
+        #---------------1st part:Company logo and name --------------------------------------------------------
+        name_frame=Frame(self,width=550,height=70,bg="#7CB9E8")
+        name_frame.place(x=0,y=0)
+        company_name=Label(name_frame,text="Astra Softwares",font=("Arial",20,"bold"),bg="#7CB9E8")
+        company_name.place(x=20,y=5)
+        page_short_discription=Label(name_frame,text="Activate Your Software",bg="#7CB9E8",font=(10))
+        page_short_discription.place(x=20,y=40)
         
-        heading_label=Label(activation_page_body,text="Activate Your Software",font=("Arial",30,"bold"),bg="#DCDCDC")
-        heading_label.place(x=50,y=0)
+        image=Image.open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'software_icon.png'))
+        resize_image=image.resize((100,60))
+        image_object=ImageTk.PhotoImage(resize_image)
+        photo_icon=Label(name_frame,image=image_object)
+        photo_icon.image=image_object
+        photo_icon.place(x=350,y=4)
 
-        message_label=Label(activation_page_body,text="Before we move on please enter license key,",
-                            font=("Times New Roman",20),bg="#DCDCDC")
-        message_label.place(x=0,y=50)
-        message_label2=Label(activation_page_body,text="so we can confirm your purchase.",
-                            font=("Times New Roman",20),bg="#DCDCDC")
-        message_label2.place(x=0,y=80)
+
+        #-----------------------2nd part : Entry widget---------------------------------------------------------------
+
+        activation_page_body=Frame(self,bg="#DCDCDC",width=550,height=280)
+        activation_page_body.place(x=0,y=71)
+
+        """
+        heading_label=Label(activation_page_body,text="Activate Your Software",font=("Arial",30,"bold"),bg="#DCDCDC")
+        heading_label.place(x=50,y=0)"""
+
+        message_label=Label(activation_page_body,text="Before we move on please enter license key,so we can confirm ",
+                            font=("Times New Roman",15),bg="#DCDCDC")
+        message_label.place(x=0,y=0)
+        message_label2=Label(activation_page_body,text="your purchase.",
+                            font=("Times New Roman",15),bg="#DCDCDC")
+        message_label2.place(x=0,y=20)
 
         license_key_label=Label(activation_page_body,text="Enter license key:",bg="#DCDCDC",font=(70))
-        license_key_label.place(x=0,y=120)
+        license_key_label.place(x=0,y=50)
 
         key=tk.StringVar()
         key.set('XXXX-XXXX')
         license_key_entry=Entry2(activation_page_body,textvariable=key,font=(15))
-        license_key_entry.place(x=130,y=120,width=300)
+        license_key_entry.place(x=130,y=50,width=300)
+
+        back_button = Button(activation_page_body, text="I don't have a key", relief="flat", width=10,activeforeground="red",
+                             cursor="hand2")
+        back_button.place(x=130, y= 80,width=100)
 
 
-
-
-
-
-
-        #-----------------------2nd part:Button settings---------------------------------------------------------------     
+        #-----------------------3rd part:Button settings---------------------------------------------------------------     
         back_button = Button(self, text="back", relief="groove", width=10)
         back_button.place(x=370, y=360)
         
@@ -196,16 +217,29 @@ class Software_activation_page(Frame):
     def verify_key():
         pass
 
-class Name_registration_page:
-    pass
+class Name_registration_page(Frame):
+    """
+    This frame is used to get the hospital name 
+    this frame has three parts:
+    *one for company name 
+    """
+    def __init__(self,window_object):
+        self.window_oject=window_object
+        super().__init__(window_object,bg="light grey",width=550,height=400)
+        self.pack(fill="x")
 
-class DBMS_setup_page:
+
+
+
+    
+
+class DBMS_setup_page(Frame):
     pass
     
-class Root_setup_page:
+class Root_setup_page(Frame):
     pass
 
-class Cloud_setup_page:
+class Cloud_setup_page(Frame):
     pass
 
     
