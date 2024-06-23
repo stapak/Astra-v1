7a22c15f09d0ec34432d1c403386583dad924cc6
stapak <125467248+stapak@users.noreply.github.com>
2024-06-23 23:57:32 +0530

    Software setup working correctly. Completed all functions related to proper working of software setup.
    *Added 'Start_setup_function' which initializes all 'setup_software'
     frame.
    
    *Created and added 'mediator functions' to all frame objects that acts
     as mediator for data transfer between Software UI functions and
     software backend funtions.
    
    *Added 'user_data' and 'hospital_name' global variable to transfer data
     from one mediator function to other mediator functions.
    
    *Completed 'SwitchPages' class's methods that are used to control
     switching of pages from one to another.

--- software_boot(Main_file).py
+++ software_boot(Main_file).py
@@ -1,24 +1,24 @@
 """
-This file is used to start the software.
+This file has functions to start in various formats such as 'setup software','run software' etc.
 """
 
 #-------------------------------------------------- Modules ---------------------------------------------------------------------------------
 
 #----------------------------------Built in liabraries--------------
+
 import json
-from turtle import setup
 import mysql.connector
+
 
 
 #-------------------------- Astra modules ------------------------------------------
 
 # Backend Modules 
-from Software_backend import *
-from Software_backend.software_setup import Database_setup,Software_setup
+from Software_backend.software_setup import Software_setup
 
 
 #Frontend Modules 
-from Software_UI.setup_frames import *
+import Software_UI.setup_frames as setup_frames
 from Software_UI.software_windows import Window
 
 
@@ -28,7 +28,13 @@ from Software_UI.software_windows import Window
 
 #-------------------- setup page related funtions --------------------------------
 
-setup_frames_list=[]
+"""
+Created a common frames list which is base for all software function depending on setup or work frames are 
+added to this list by other function.
+
+"""
+frames_list=[]
+
 current_page=0
 class SwitchPages:
     """
@@ -36,20 +42,93 @@ class SwitchPages:
     """
     @staticmethod
     def switch_back():
-        frame=setup_frames_list[current_page-1]
-        frame.tkrise()
+        global current_page
+        current_page=current_page-1
+        frame=frames_list[current_page]
+        frame.tkraise()
+        frame.place(x=0,y=0)
+        
     
     @staticmethod
     def switch_front():
-        frame=setup_frames_list[current_page+1]
-        frame.tkrise()
+        global current_page
+        current_page=current_page+1
+        frame=frames_list[current_page]
+        frame.tkraise()
+        frame.place(x=0,y=0)
+        
         
 
-window=Window()
-window_root=window.setup_window()
-setup_page1_object=Welcome_page(window_root,SwitchPages)
-setup_frames_list.append(setup_page1_object)
-print(setup_frames_list)
+def start_setup():
+    """
+    Function used to pack pages by creating class objects and append them in frames list
+    """
+    #Initailizing window object
+    window=Window()
+    window_object=window.setup_window()
+    
+    #Initializing 1st page.
+    setup_page_1=setup_frames.Welcome_page(window_object,SwitchPages)
+    
+    #Initailizing 2nd page.
+    setup_page_2=setup_frames.Terms_condition_page(window_object,SwitchPages)
+    
+    #Initailizing 3rd page.
+    def mediator_function_3rd(key):
+        return Software_setup.license_key_verification(key)
+    setup_page_3=setup_frames.Software_activation_page(window_object,SwitchPages,verify_function=mediator_function_3rd)
+    
+    #Initializing 4th page
+    global hospital_name
+    def mediator_function_4th(name):
+        global hospital_name
+        hospital_name=name
+        Software_setup.register_hospital_name(name)
+    setup_page_4=setup_frames.Name_registration_page(window_object,SwitchPages,register_name=mediator_function_4th)
+    
+    #Initializing 5th page
+    user_data={}
+    def mediator_function_5th(id,password,host):
+        global user_data
+        
+        user_data={'host':host,
+                   'user id':id,'user password':password}
+        return Software_setup.database_verification(**user_data)
+        
+    setup_page_5=setup_frames.Admin_setup_page(window_object,SwitchPages,DBMS_verification=mediator_function_5th)
+
+    #Initializing 6th page.
+    def mediator_function_6th():
+        global user_data
+        global hospital_name
+        user_data['hospital name']=hospital_name
+        return Software_setup.setup_database(**user_data)
+    setup_page_6=setup_frames.DBMS_setup_page(window_object,SwitchPages,dbms_setup_function=mediator_function_6th)
+    
+    #Initializing 7th page.
+    def mediator_function_8th():
+        frames_list[0:6]
+        del frames_list[0]#this line will be executed after all elements of list are deleted hence last page will be of index 0.
+        return None
+    setup_page_7=setup_frames.setup_finish_page(window_object,finish_setup=mediator_function_8th)
+
+    # Adding every frame to list.
+    frames_list.append(setup_page_1)
+    frames_list.append(setup_page_2)
+    frames_list.append(setup_page_3)
+    frames_list.append(setup_page_4)
+    frames_list.append(setup_page_5)
+    frames_list.append(setup_page_6)
+    frames_list.append(setup_page_7)
+   
+   
+    
+    window_object.mainloop()
+    
+    
+    
+
+    
 
 
 
@@ -66,4 +145,6 @@ else:
 
 
 
-
+if __name__=='__main__':
+    start_setup()
+    pass
